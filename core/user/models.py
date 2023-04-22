91dd3604875f0bd9e008151f644294c5368a802a
alikoc <akoc3273@gmail.com>
2023-04-23 00:25:15 +0300

    Initial commit

---
+++ core/user/models.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if email is None:
+            raise TypeError('Users must have an email.')
+
+        now = timezone.now()
+        user = self.model(
+            email=self.normalize_email(email),
+            is_active=True,
+            is_verified=True,
+            last_login=now,
+            date_joined=now,
+            **kwargs
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+        if email is None:
+            raise TypeError('Superusers must have an email.')
+
+        user = self.create_user(email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        db_table = "users"
