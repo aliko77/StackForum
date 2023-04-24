346baeb13e660d82250e3c1b8b997ddf388658e8
aliko77 <akoc3273@gmail.com>
2023-04-25 02:03:48 +0300

    commit

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+
+const HelloWorld: FC = () => {
+    return (
+        <div>
+            <h1 className="text-2xl text-red-500">
+                HelloWorld
+            </h1>
+        </div>
+    );
+}
+export default HelloWorld
