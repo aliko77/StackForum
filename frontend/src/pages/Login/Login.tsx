35e1b2de3435cbb664b01955d4943f914d1e6c9d
alikoc <akoc3273@gmail.com>
2023-04-27 20:28:29 +0300

    commit

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,9 +1,17 @@
 import React, {FC} from 'react';
+import Logo from "../../components/Logo";
 
 const Login: FC = () => {
     return (
-        <div>
-            Hi
+        <div className="mx-auto w-full max-w-lg p-3 sm:my-20 my-10">
+            <div className="border rounded p-3 bg-white dark:text-gray-100 dark:bg-night-e dark:border-gray-200">
+                <div className="flex items-center justify-center mb-4">
+                    <Logo/>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     );
 }
