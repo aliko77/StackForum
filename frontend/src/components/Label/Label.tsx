fde8410e8aef657abb3c9d9e50b45c567364bcea
aliko77 <akoc3273@gmail.com>
2023-04-29 01:22:54 +0300

    commit

--- frontend/src/components/Label/Label.tsx
+++ frontend/src/components/Label/Label.tsx
@@ -8,7 +8,7 @@ type LabelProps = {
 const Label: FC<LabelProps> = ({htmlFor, value}) => {
     return (
         <label htmlFor={htmlFor}
-               className="block mb-2 font-medium text-gray-900 dark:text-white">
+               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             {value}
         </label>
     );
