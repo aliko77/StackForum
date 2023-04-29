692bc2ab4be8a718c348ba5e969e47009acf8455
alikoc <akoc3273@gmail.com>
2023-04-29 15:04:05 +0300

    0

--- frontend/src/components/TextInput/TextInput.tsx
+++ frontend/src/components/TextInput/TextInput.tsx
@@ -1,18 +1,32 @@
-import {FC} from 'react';
+import { FC } from "react";
 
 interface InputProps {
-    id: string,
-    type: string,
-    placeholder: string,
-    required: boolean
+    id: string;
+    type: string;
+    placeholder: string;
+    required: boolean;
+    value?: string;
+    onChange?: (e: any) => void;
 }
 
-const TextInput: FC<InputProps> = ({id, type, placeholder, required}) => {
+const TextInput: FC<InputProps> = ({
+    id,
+    type,
+    placeholder,
+    required,
+    value,
+    ...props
+}) => {
     return (
-        <input type={type} id={id} placeholder={placeholder} required={required}
-               className="block w-full p-2 outline-0 bg-gray-50 border border-gray-300 text-gray-900 rounded-sm focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+        <input
+            type={type}
+            id={id}
+            placeholder={placeholder}
+            required={required}
+            className="block w-full p-2 outline-0 bg-gray-50 border border-gray-300 text-gray-900 rounded-sm focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+            {...props}
         />
     );
-}
+};
 
 export default TextInput;
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,44 +1,81 @@
-import {FC} from 'react';
+import { FC } from "react";
 import Logo from "../../components/Logo";
 import Label from "../../components/Label";
 import TextInput from "../../components/TextInput";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Button from "../../components/Button";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+    email: Yup.string()
+        .email("Geçersiz e-mail adresi")
+        .required("*Zorunlu alan"),
+    password: Yup.string().required("*Zorunlu alan"),
+});
 
 const Login: FC = () => {
+    const LoginForm = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema,
+        onSubmit: (values) => {
+            console.log(values);
+        },
+    });
+
     return (
         <div className="mx-auto w-full max-w-sm p-3 sm:my-20 my-10">
             <div className="flex items-center justify-center mb-4">
-                <Logo noText/>
+                <Logo noText />
             </div>
-            <div
-                className="border rounded p-3 bg-white dark:text-gray-100 dark:bg-night-e dark:border-gray-500">
+            <div className="border rounded p-3 bg-white dark:text-gray-100 dark:bg-night-e dark:border-gray-500">
                 <div>
-                    <form action="#" noValidate>
+                    <form onSubmit={LoginForm.handleSubmit}>
                         <div className="mb-3">
-                            <Label htmlFor="email" value="Email"/>
-                            <TextInput id={"email"} type={"text"} placeholder={"name@stack.com"}
-                                       required={true}/>
+                            <Label htmlFor="email" value="Email" />
+                            <TextInput
+                                id={"email"}
+                                type={"email"}
+                                placeholder={"name@stack.com"}
+                                required={true}
+                                value={LoginForm.values.email}
+                                onChange={LoginForm.handleChange}
+                            />
                         </div>
                         <div className="mb-3">
-                            <Label htmlFor="password" value="Şifre"/>
-                            <TextInput id={"password"} type={"password"} placeholder={"•••••••••"}
-                                       required={true}/>
+                            <Label htmlFor="password" value="Şifre" />
+                            <TextInput
+                                id={"password"}
+                                type={"password"}
+                                placeholder={"•••••••••"}
+                                required={true}
+                                value={LoginForm.values.password}
+                                onChange={LoginForm.handleChange}
+                            />
                         </div>
                         <div className="flex justify-end mb-3">
-                            <NavLink to="/forgot-password"
-                                     className="text-sm font-medium text-rose-400 hover:underline dark:text-indigo-500">
+                            <NavLink
+                                to="/forgot-password"
+                                className="text-sm font-medium text-rose-400 hover:underline dark:text-indigo-500"
+                            >
                                 Şifreni mi unuttun?
                             </NavLink>
                         </div>
                         <div>
-                            <Button text={"Giriş yap"} sClass="w-full bg-rose-500 hover:bg-rose-600 dark:bg-indigo-500 dark:hover:bg-indigo-600" type="submit"/>
+                            <Button
+                                text={"Giriş yap"}
+                                sClass="w-full bg-rose-500 hover:bg-rose-600 dark:bg-indigo-500 dark:hover:bg-indigo-600"
+                                type="submit"
+                            />
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Login;
