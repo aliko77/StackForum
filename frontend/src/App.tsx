f6ad9a2c6d3910ab0b3cba7aef407381de5cf881
aliko77 <akoc3273@gmail.com>
2023-05-05 21:14:38 +0300

    :)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+   return (
+      <div>
+         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+         <div className="bg-night-100">
+            <p>Test</p>
+         </div>
+         <div className="bg-night-200">
+            <p>Test</p>
+         </div>
+      </div>
+   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+   darkMode: 'class',
+   theme: {
+      extend: {
+         colors: {
+            night: {
+               100: '#383b50',
+               200: '#323448',
+            },
+         },
+         animation: {
+            bounce200: 'bounce 1s infinite 200ms',
+            bounce400: 'bounce 1s infinite 400ms',
+         },
+      },
+   },
+   plugins: [],
+};
