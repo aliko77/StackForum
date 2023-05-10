4432bad2ff0fc93d5d0f14c8a6f70091a3f1bdaf
alikoc <akoc3273@gmail.com>
2023-05-10 15:01:40 +0300

    :/

--- core/auth/serializers.py
+++ core/auth/serializers.py
@@ -10,40 +10,40 @@ from core.user.serializers import UserSerializer
 
 
 class LoginSerializer(TokenObtainPairSerializer):
-   def validate(self, attrs):
-      data = super().validate(attrs)
-      refresh = self.get_token(self.user)
-      data['user'] = UserSerializer(self.user).data
-      data['refreshToken'] = str(refresh)
-      data['accessToken'] = str(refresh.access_token)
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data['user'] = UserSerializer(self.user).data
+        data['refreshToken'] = str(refresh)
+        data['accessToken'] = str(refresh.access_token)
 
-      if api_settings.UPDATE_LAST_LOGIN:
-         update_last_login(None, user=self.user)
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, user=self.user)
 
-      return data
+        return data
 
 
 class RegisterSerializer(UserSerializer):
-   password = serializers.CharField(
-      max_length=128, min_length=8, write_only=True, required=True)
-   email = serializers.EmailField(
-      required=True, write_only=True, max_length=128)
+    password = serializers.CharField(
+        max_length=128, min_length=8, write_only=True, required=True)
+    email = serializers.EmailField(
+        required=True, write_only=True, max_length=128)
 
-   class Meta:
-      model = User
-      fields = ['id', 'email', 'password']
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password']
 
-   def create(self, validated_data):
-      try:
-         User.objects.get(email=validated_data['email'])
-         raise ValidationError({'error': 'This email is already taken.'})
-      except ObjectDoesNotExist:
-         user_data = {
-            'email': validated_data['email'],
-            'password': validated_data['password'],
-            'confirmPassword': validated_data['confirmPassword'],
-         }
-         user = User.objects.create_user(**user_data)
-         if user:
-            pass
-      return user
+    def create(self, validated_data):
+        try:
+            User.objects.get(email=validated_data['email'])
+            raise ValidationError({'error': 'This email is already taken.'})
+        except ObjectDoesNotExist:
+            user_data = {
+                'email': validated_data['email'],
+                'password': validated_data['password'],
+                'confirmPassword': validated_data['confirmPassword'],
+            }
+            user = User.objects.create_user(**user_data)
+            if user:
+                pass
+        return user
