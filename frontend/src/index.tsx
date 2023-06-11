629bd6056c05a737999ee76d42b2d8636e35dd9c
alikoc <akoc3273@gmail.com>
2023-06-11 22:08:12 +0300

    some fixes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import Router from 'routes/Router';
+import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-   <RouterProvider router={Router} />,
+   <StrictMode>
+      <RouterProvider router={Router} />
+   </StrictMode>,
 );
