a14fbf879ce74f74f569640a772a867b4692e51c
alikoc <akoc3273@gmail.com>
2023-06-17 12:23:25 +0300

    new feats

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -6,9 +6,10 @@ import classNames from 'classnames';
 type LogoProps = {
    noText?: boolean;
    noRedirect?: boolean;
+   hw?: string;
 };
 
-export const Logo: FC<LogoProps> = ({ noText, noRedirect }) => {
+export const Logo: FC<LogoProps> = ({ noText, noRedirect, hw = '8' }) => {
    const _ = (): ReactNode => {
       const logoClasses = classNames(
          'block',
@@ -19,9 +20,21 @@ export const Logo: FC<LogoProps> = ({ noText, noRedirect }) => {
          { 'sr-only': noText },
       );
 
+      const hwList: { [key: string]: string } = {
+         '2': 'h-2 w-2',
+         '4': 'h-4 w-4',
+         '6': 'h-6 w-6',
+         '8': 'h-8 w-8',
+         '10': 'h-10 w-10',
+         '12': 'h-12 w-12',
+         '14': 'h-14 w-14',
+         '16': 'h-16 w-16',
+      };
+      const imgClasses = classNames(hwList[hw]);
+
       return (
          <div className="flex space-x-2 items-center">
-            <img src={StackLogo} alt="Logo" className="h-8 w-8" />
+            <img src={StackLogo} alt="Logo" className={imgClasses} />
             <h1 className={logoClasses}>
                <span>Stack</span>
                <span className="font-semibold ml-0.5">Forum</span>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,12 +1,41 @@
-import { useAuth } from 'hooks/useAuth';
+import { Logo } from 'components/Logo';
 import { FC } from 'react';
 
 const Home: FC = () => {
-   const { user } = useAuth();
    return (
-      <div className="m-auto text-center">
-         <div className="text-xl dark:text-gray-100">Hoşgeldin: {user?.username || 'Misafir'}</div>
-      </div>
+      <>
+         <div>
+            <div id="topics" className="container mx-auto py-8">
+               <div className="flex justify-between items-center bg-night-900 rounded-t p-3 border-b border-gray-500">
+                  <h1 className="text-base font-semibold tracking-wide text-gray-300">
+                     Konu Başlıkları
+                  </h1>
+                  <div>
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5 text-gray-300"
+                     >
+                        <path
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+                        />
+                     </svg>
+                  </div>
+               </div>
+               <div className="flex items-center align-middle bg-night-900 border-b border-gray-500 text-gray-100">
+                  <div className="p-3 border-r border-gray-500">
+                     <Logo noText noRedirect hw="12" />
+                  </div>
+                  <div className="p-3 w-full">Test</div>
+               </div>
+            </div>
+         </div>
+      </>
    );
 };
 
